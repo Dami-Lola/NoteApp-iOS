fdbff43af9c443ba22a4da7fa1137df564450356
Dami-lola <damiokafor@gmail.com>
2020-12-01 22:58:15 +0100

    base project

---
+++ NoteApp/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  NoteApp
+//
+//  Created by Damilola Okafor on 12/1/20.
+//  Copyright © 2020 Damilola Okafor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    let context = (UIApplication.shared.delegate as! AppDelegate)
+    .persistentContainer.viewContext
+    
+    let notes = [Notes]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       
+        return notes.count
+    }
+    
+    
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    // MARK: - Data manipluations
+    
+    
+    // MARK: - Table view Delegate methods
+    
+    // MARK: - Add Notes
+
+}
+
